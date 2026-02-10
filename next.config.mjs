d784a4ab957d35249ba5bd4a254f0e3054a9b69a
Asif Ahamed <168105607+asifahamed11@users.noreply.github.com>
2026-02-11 03:24:39 +0600

    Fix formatting issues in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  ে
+  output: "export",
   basePath: "/asif-ahamed-portfolio",
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
